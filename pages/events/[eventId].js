149246f834d52c84f15784ab205705b02f16c204
aswathyrajm <aswathyraj767@gmail.com>
2021-05-24 20:55:44 +0530

    events details added

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,9 @@
 import { useRouter } from "next/router";
+import React from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+import { getEventById } from "../../dummy-data.js";
 
 export default function EventDetailsPage() {
   const router = useRouter();
@@ -8,8 +13,17 @@ export default function EventDetailsPage() {
     return <p>No events found!</p>;
   }
   return (
-    <div>
-      <h1>Event details</h1>
-    </div>
+    <React.Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </React.Fragment>
   );
 }
