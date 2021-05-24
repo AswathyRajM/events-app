d8fb3ed240b2a2bf38712b70d5bd42cc5e60e7d6
aswathyrajm <aswathyraj767@gmail.com>
2021-05-24 20:41:13 +0530

    events item added

--- components/EventsList.js
+++ components/EventsList.js
@@ -1,10 +1,11 @@
 import React from "react";
 import EventItem from "./EventItem";
+import classes from './eventList.module.css'
 
 export default function EventsList(props) {
   const { events } = props;
   return (
-    <ul>
+    <ul className={classes.list}>
       {events.map((event) => {
         return <EventItem
           key={event.id}
--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,12 @@
+import { useRouter } from "next/router";
+
 export default function EventDetailsPage() {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+  if (!event) {
+    return <p>No events found!</p>;
+  }
   return (
     <div>
       <h1>Event details</h1>
